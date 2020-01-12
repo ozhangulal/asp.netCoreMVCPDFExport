08aec615c0bc6083f3fe6ad064128a51db23c90e
ozhangulal <ozhangulal@outlook.com>
2020-01-12 22:08:02 +0300

    asp.netCoreMVCPDFExport
    export of cshtml page created with table tags as pdf using Rotativa

---
+++ CoreMVCExportPDF/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CoreMVCExportPDF.Models;
+using Rotativa.AspNetCore;
+
+namespace CoreMVCExportPDF.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult LeagueTable()
+        {
+            LeagueTable nbaLeague = new LeagueTable();
+            nbaLeague.createTableInstance();
+
+            return View(nbaLeague);
+        }
+
+        public IActionResult LeaguePDF()
+        {
+            LeagueTable nbaLeague = new LeagueTable();
+            nbaLeague.createTableInstance();
+
+
+            return new ViewAsPdf("LeagueTable", nbaLeague)
+            {
+                CustomSwitches = "--footer-center [page]"
+            };
+        }
+    }
+}
---
+++ CoreMVCExportPDF/Models/LeagueTable.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMVCExportPDF.Models
+{
+    public class LeagueTable
+    {
+        public DateTime DateCreated { get; set; }
+        public int gameWeek { get; set; }
+
+        public List<LeagueRow> rows { get; set; }
+
+        public LeagueTable()
+        {
+            this.DateCreated = DateTime.Now.Date;
+            this.rows = new List<LeagueRow>();
+            this.gameWeek = 33;
+        }
+
+        public void createTableInstance()
+        {
+            string[] teams = { "Atlanta Hawks", "Boston Celtics", "Brooklyn Nets", "Charlotte Hornets", "Chicago Bulls", "Clevland Cavaliers", "Dallas Mavericks", "Denver Nuggets",
+                               "Detroit Pistons", "Golden State Warriors", "LA Clippers", "LA Lakers", "Milwaukee Bucks", "Minnesota Timberwolves", "New Orleans Pelicans",
+                                "Philadephina 76ers", "Phoenix Suns", "Toronto Raptors", "Sacremento Kings", "Orlando Magic"};
+
+            int[] gamesPlayed = new int[] { 33, 33, 33, 34, 33, 34, 33, 34, 33, 33, 33, 34, 33, 34, 33, 34, 33, 33, 33, 34 };
+
+            Random rnd = new Random();
+            int[] accesses = Enumerable.Range(0, 20).OrderBy(c => rnd.Next()).ToArray();
+
+
+            int[] points = new int[] { 82, 80, 66, 64, 63, 61, 47, 47, 46, 46, 42, 39, 38, 36, 35, 33, 33, 28, 17, 14 };
+
+
+            for (int i = 0; i < gamesPlayed.Length; i++)
+            {
+                rows.Add(new LeagueRow(i + 1, teams[accesses[i]], gamesPlayed[accesses[i]], points[i]));
+            }
+        }
+    }
+}
